4d6f49d9f8f0e7eeed7097c9cdeca8cd2da3d3cc
ZENGHU <978689162@qq.com>
2019-09-22 22:32:15 +0800

    创建项目

---
+++ src/main/java/tiger/ApplicationConfig.java
@@ -0,0 +1,97 @@
+package tiger;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.*;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@PropertySource(value = {"classpath:application.properties"})
+@ComponentScan(basePackages = {"tiger"})
+@EnableKafka
+public class ApplicationConfig {
+
+    @Value("${kafka.bootstrap-servers}")
+    private String bootstrapServers;
+    @Value("${kafka.consumer.group.id}")
+    private String groupId;
+
+    @Value("${kafka.consumer.max.poll.records}")
+    private int maxPollRecords;
+
+    @Value("${kafka.consumer.enable.auto.commit}")
+    private boolean enableAutoCommit;
+
+    @Value("${kafka.consumer.auto.commit.interval.ms}")
+    private int autoCommitIntervalMs;
+
+    @Value("${kafka.consumer.auto.offset.reset}")
+    private String autoOffsetReset;
+
+    @Value("${kafka.consumer.key.deserializer}")
+    private String keyDeserializerClass;
+
+    @Value("${kafka.consumer.value.deserializer}")
+    private String valueDeserializerClass;
+
+    @Value("${kafka.consumer.topics}")
+    private String[] topics;
+
+    @Value("${kafka.consumer.concurrency}")
+    private int concurrency;
+
+    @Value("${kafka.consumer.batch-listener}")
+    private boolean batchListener;
+
+
+    @Bean
+    public Map<String, Object> consumerConfig() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, maxPollRecords);
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, enableAutoCommit);
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, autoOffsetReset);
+        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, autoCommitIntervalMs);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, keyDeserializerClass);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, valueDeserializerClass);
+        return props;
+    }
+
+    @Bean
+    public ConsumerFactory consumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(consumerConfig());
+    }
+
+    @Bean
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    public Consumer kafkaConsumer() {
+        Consumer consumer = consumerFactory().createConsumer();
+        consumer.subscribe(Arrays.asList(topics));
+        return consumer;
+    }
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory listenerContainerFactory() {
+
+        ConcurrentKafkaListenerContainerFactory containerFactory = new ConcurrentKafkaListenerContainerFactory();
+        containerFactory.setConcurrency(concurrency);
+        containerFactory.setBatchListener(batchListener);
+        containerFactory.setConsumerFactory(consumerFactory());
+        containerFactory.afterPropertiesSet();
+        return containerFactory;
+
+
+    }
+
+
+}
